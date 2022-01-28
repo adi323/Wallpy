6e3d448e0b8291f07fbe3509bf5c8593fca07459
Rajib Lochan Nandi <adityanandi550@gmail.com>
2022-01-28 20:00:33 +0530

    Fixes

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,23 +3,20 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
+  "assets/AssetManifest.json": "e9fb545adf37161df7edab8dadc86f7f",
+"assets/assets/prfimg.jpg": "604e284a6bc48ecd0244d46071618963",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
 "assets/NOTICES": "2cdacc91802efd4cfbfbab9def79214e",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
-"canvaskit/canvaskit.js": "43fa9e17039a625450b6aba93baf521e",
-"canvaskit/canvaskit.wasm": "04ed3c745ff1dee16504be01f9623498",
-"canvaskit/profiling/canvaskit.js": "f3bfccc993a1e0bfdd3440af60d99df4",
-"canvaskit/profiling/canvaskit.wasm": "a9610cf39260f60fbe7524a785c66101",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "ed22bc908a62d8d411c04cd00068f657",
-"/": "ed22bc908a62d8d411c04cd00068f657",
-"main.dart.js": "537f0e84830f892ff302b9af98f05186",
+"index.html": "efacac1fa035a1b167630b5bdbdaf96a",
+"/": "efacac1fa035a1b167630b5bdbdaf96a",
+"main.dart.js": "b0d410a04e4a42c0d7f2cc905809c715",
 "manifest.json": "83d533e5ab22a8928a684b90a19bb48d",
 "version.json": "389a6e6e6f34312743ca5f36467c84b5"
 };
